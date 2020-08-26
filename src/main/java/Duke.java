d96465bbc5a19f0c32e458b5c5640734159e5c19
yellow-fellow <huangshaohang.98@gmail.com>
2020-08-26 23:59:51 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("---------------------------------------");
-        System.out.println("Hello! I'm Duke!");
+        System.out.println("Hello! Welcome to yellow-fellow bot :>");
         System.out.println("What can I do for you?");
         System.out.println("---------------------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("---------------------------------------");
+        boolean echo = true;
+        while (echo){
+            Scanner userInput = new Scanner(System.in);
+            System.out.println("Enter a word: ");
+            String userInputString = userInput.nextLine();
+            if (userInputString.equals("bye")){
+                echo = false;
+                System.out.println("---------------------------------------");
+                System.out.println("What a shame :( See you again soon!");
+                System.out.println("---------------------------------------");
+            }
+            else{
+                System.out.println("---------------------------------------");
+                System.out.println("You typed " + userInputString);
+                System.out.println("---------------------------------------");
+            }
+        }
     }
+
 }
