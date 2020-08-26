ab02aa7416c9327b1fc5a61bb644a498d2778fd9
yellow-fellow <huangshaohang.98@gmail.com>
2020-08-27 01:06:28 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,26 +1,73 @@
 import java.util.*;
 
 public class Duke {
+    static class task{
+        private Boolean completion = false;
+        private String taskName;
+
+        task(){
+            this.completion = completion;
+            this.taskName = taskName;
+        }
+
+        void completeTask(){
+            this.completion = true;
+        }
+
+        boolean getTaskStatus(){
+            return this.completion;
+        }
+
+        String getTaskName(){
+            return this.taskName;
+        }
+
+        void setTaskName(String input){
+            this.taskName = input;
+        }
+    }
     public static void main(String[] args) {
         System.out.println("---------------------------------------");
         System.out.println("Hello! Welcome to yellow-fellow bot :>");
         System.out.println("What can I do for you?");
         System.out.println("---------------------------------------");
-        String[] items = new String[100];
+        task[] items = new task[100];
         Scanner userInput = new Scanner(System.in);
         System.out.println("Enter a word: ");
         String userInputString = userInput.nextLine();
         int itemCount = 0;
-        while(!(userInputString.equals("list"))){
-            items[itemCount] = userInputString;
+        int noOfTasksCompleted = 0;
+        while(!(userInputString.equals("exit") || userInputString.equals("quit"))){
+            task temp = new task();
+            temp.setTaskName(userInputString);
+            items[itemCount] = temp;
             itemCount += 1;
             userInput = new Scanner(System.in);
             System.out.println("Enter a word: ");
             userInputString = userInput.nextLine();
+            if (userInputString.equals("done")){
+                Scanner userInputCompleteTask = new Scanner(System.in);
+                System.out.println("Please key in the number of the task you have completed: ");
+                String userInputCompleteTaskString = userInput.nextLine();
+                int userInputCompleteTaskInt = Integer.parseInt(userInputCompleteTaskString);
+                items[userInputCompleteTaskInt-1].completeTask();
+                noOfTasksCompleted += 1;
+                userInput = new Scanner(System.in);
+                System.out.println("Enter a word: ");
+                userInputString = userInput.nextLine();
+            }
         }
         System.out.println("---------------------------------------");
+        System.out.println("You have " + itemCount + " tasks and completed " + noOfTasksCompleted + " in total. Congratulations! :>");
         for (int i=1;i<itemCount+1;i++){
-            System.out.println(i + "." + items[i-1]);
+            System.out.print(i + ".");
+            if (items[i-1].getTaskStatus() == false){
+                System.out.print("[✗] ");
+            }
+            else{
+                System.out.print("[✓] ");
+            }
+            System.out.println(items[i-1].getTaskName());
         }
         System.out.println("---------------------------------------\n\n");
 
