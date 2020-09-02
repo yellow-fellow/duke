0ae29eb8e8aec5e24345e9176b6f069b72224b67
yellow-fellow <huangshaohang.98@gmail.com>
2020-09-03 02:07:28 +0800

    Incremented to Level 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,7 @@ public class Duke {
     static class task{
         private Boolean completion = false;
         private String taskName;
+        private String date;
 
         task(){
             this.completion = completion;
@@ -25,7 +26,60 @@ public class Duke {
         void setTaskName(String input){
             this.taskName = input;
         }
+
+        public String returnDate(){ return this.date; }
+
+        String getClassName(){ String className = this.getClass().getSimpleName(); return className;}
     }
+    public static class todo extends task{
+        private String taskName;
+        public todo(String taskName){
+            this.taskName = taskName;
+        }
+        String getTaskName(){
+            return this.taskName;
+        }
+
+        public String getClassName(){ String className = this.getClass().getSimpleName(); return className;}
+    }
+
+    public static class deadline extends task{
+        private String taskName;
+        private String date;
+
+        public deadline (String taskName, String date){
+            this.taskName = taskName;
+            this.date = date;
+        }
+
+        String getTaskName(){
+            return this.taskName;
+        }
+
+        public String returnDate(){ return this.date; }
+
+        public String getClassName(){ String className = this.getClass().getSimpleName(); return className;}
+
+    }
+    public static class event extends task{
+        private String taskName;
+        private String date;
+
+        public event (String taskName, String date){
+            this.taskName = taskName;
+            this.date = date;
+        }
+
+        String getTaskName(){
+            return this.taskName;
+        }
+
+        public String returnDate(){ return this.date; }
+
+        public String getClassName(){ String className = this.getClass().getSimpleName(); return className;}
+
+    }
+
     public static void main(String[] args) {
         System.out.println("---------------------------------------");
         System.out.println("Hello! Welcome to yellow-fellow bot :>");
@@ -33,17 +87,44 @@ public class Duke {
         System.out.println("---------------------------------------");
         task[] items = new task[100];
         Scanner userInput = new Scanner(System.in);
-        System.out.println("Enter a word: ");
+        System.out.println("Enter an instruction: \nPlease select either todo / deadline / event / list / quit.");
         String userInputString = userInput.nextLine();
         int itemCount = 0;
         int noOfTasksCompleted = 0;
         while(!(userInputString.equals("exit") || userInputString.equals("quit"))){
-            task temp = new task();
-            temp.setTaskName(userInputString);
-            items[itemCount] = temp;
-            itemCount += 1;
+            if (userInputString.equals("todo")){
+                userInput = new Scanner(System.in);
+                System.out.println("Please enter your todo task. :)");
+                userInputString = userInput.nextLine();
+                todo temp = new todo(userInputString);
+                items[itemCount] = temp;
+                itemCount += 1;
+            }
+            if (userInputString.equals("deadline")){
+                Scanner userInput1 = new Scanner(System.in);
+                System.out.println("Please enter your deadline task. :)");
+                String userInputString1 = userInput.nextLine();
+                Scanner userInput2 = new Scanner(System.in);
+                System.out.println("Please enter your deadline. :)");
+                String userInputString2 = userInput.nextLine();
+                deadline temp = new deadline(userInputString1,userInputString2);
+                items[itemCount] = temp;
+                itemCount += 1;
+            }
+
+            if (userInputString.equals("event")){
+                Scanner userInput1 = new Scanner(System.in);
+                System.out.println("Please enter your event task. :)");
+                String userInputString1 = userInput.nextLine();
+                Scanner userInput2 = new Scanner(System.in);
+                System.out.println("Please enter your event duration. :)");
+                String userInputString2 = userInput.nextLine();
+                event temp = new event(userInputString1,userInputString2);
+                items[itemCount] = temp;
+                itemCount += 1;
+            }
             userInput = new Scanner(System.in);
-            System.out.println("Enter a word: ");
+            System.out.println("Enter an instruction: \nPlease select either todo / deadline / event / list / quit.");
             userInputString = userInput.nextLine();
             if (userInputString.equals("done")){
                 Scanner userInputCompleteTask = new Scanner(System.in);
@@ -53,7 +134,7 @@ public class Duke {
                 items[userInputCompleteTaskInt-1].completeTask();
                 noOfTasksCompleted += 1;
                 userInput = new Scanner(System.in);
-                System.out.println("Enter a word: ");
+                System.out.println("Enter an instruction: \nPlease select either todo / deadline / event / list / quit.");
                 userInputString = userInput.nextLine();
             }
         }
@@ -61,13 +142,29 @@ public class Duke {
         System.out.println("You have " + itemCount + " tasks and completed " + noOfTasksCompleted + " in total. Congratulations! :>");
         for (int i=1;i<itemCount+1;i++){
             System.out.print(i + ".");
+            if (((items[i-1]).getClassName()).equals("todo")){
+                System.out.print("[T] ");
+            }
+            if (((items[i-1]).getClassName()).equals("deadline")){
+                System.out.print("[D] ");
+            }
+            if (((items[i-1]).getClassName()).equals("event")){
+                System.out.print("[E] ");
+            }
             if (items[i-1].getTaskStatus() == false){
                 System.out.print("[✗] ");
             }
             else{
                 System.out.print("[✓] ");
             }
-            System.out.println(items[i-1].getTaskName());
+            System.out.print(items[i-1].getTaskName());
+            if (((items[i-1]).getClassName()).equals("todo")){
+                System.out.println(" ");
+            }
+            if (((items[i-1]).getClassName()).equals("deadline")||((items[i-1]).getClassName()).equals("event")){
+                System.out.println(" " + (items[i-1]).returnDate());
+            }
+
         }
         System.out.println("---------------------------------------\n\n");
 
